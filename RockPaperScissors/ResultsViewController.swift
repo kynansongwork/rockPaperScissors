282c10a4b48556b1c70336454cee09735e99a6b6
Kynan Song <kynan.song@bemo.co>
2018-04-25 14:55:00 +0100

    Added game logic and enum notes, need to fix constraints.

--- RockPaperScissors/ResultsViewController.swift
+++ RockPaperScissors/ResultsViewController.swift
@@ -12,17 +12,33 @@ enum Weapon: String {
     case Rock = "Rock"
     case Paper = "Paper"
     case Scissors = "Scissors"
+    
+    static func computerChoice() -> Weapon {
+        //has to be within the enum to be called by it.
+        let weapons = ["Rock", "Paper", "Scissors"]
+        //Putting enum values into an array
+        let pick = Int(arc4random_uniform(3))
+        //pick is a random int ranging from 0 to 3. Needs to be converted to an int.
+        return Weapon(rawValue: weapons[pick])!
+        //returns enum value based on item from weapons array.
+    }
+    
 }
 
 class ResultsViewController: UIViewController {
     
     @IBOutlet weak var playerMove: UIImageView!
+    @IBOutlet weak var result: UILabel!
     
     var userChoice: Weapon!
+    var computerMove: Weapon = Weapon.computerChoice()
+    //Has to be cast to call the function.
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         displayPlayerResult()
+        gameResult()
+        print("computer: \(computerMove)")
     }
     
     @IBAction func goBack() {
@@ -37,7 +53,24 @@ class ResultsViewController: UIViewController {
         } else if userChoice == .Paper {
             playerMove.image = UIImage(named: "paper")
         }
+    }
+    
+    func gameResult() {
+        if (userChoice == computerMove) {
+            result.text = "draw"
+        } else if (userChoice == .Rock && computerMove != .Paper) {
+            result.text = "Rock beats \(computerMove)"
+        } else if (userChoice == .Paper && computerMove != .Scissors) {
+            result.text = "Paper beats \(computerMove)"
+        } else if (userChoice == .Scissors && computerMove != .Rock) {
+            result.text = "Scissors beats \(computerMove)"
+        } else {
+            result.text = "You lose to \(computerMove)"
+        }
         
     }
+    
+
+
     
 }
